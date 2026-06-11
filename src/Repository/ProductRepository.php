edec2d8753433bdb0679379b02493af27671c8af
mohamed_bnz <simobnz81@gmail.com>
2026-06-11 15:52:50 +0100

    ajouter le code de ProductRepository pour communique avec db

---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,17 @@
+<?php
+// src/Repository/ProductRepository.php
+require_once __DIR__ . '/../../config/database.php';
+
+class ProductRepository 
+{
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getConnection();
+    }
+
+    public function getAllProducts() 
+    {
+        return $this->db->query("SELECT * FROM produits ORDER BY nom ASC")->fetchAll();
+    }
+}
